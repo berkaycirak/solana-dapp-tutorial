67432e94870d32d5e0b64705e974932b1572c2ec
Berkay ÇIRAK <81809211+berkaycirak@users.noreply.github.com>
2024-08-03 02:09:00 +0300

    Update sk_decode.ts

--- sk_decode.ts
+++ sk_decode.ts
@@ -1,7 +1,7 @@
 import bs58 from 'bs58';
 
 const kp = bs58.decode(
-  '2H4JqQ5o1gNnUL5US5hGpg7mSWHL1FTQjetoxiY15wmXwWERAQdvUobRSTQUYsaBybudFVBXt3UK45T8QYFc5kp2'
+  'privateKey'
 );
 
 console.log('My Secret key is:', kp);
